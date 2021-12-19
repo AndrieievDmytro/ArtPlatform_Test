fe10ef49756d2e7ce50f186168c807e2ab26c8b4
causesauce <adrian.chervinchuk1@gmail.com>
2021-12-19 15:41:41 +0100

    test added for" comment, reviewerrating, userrating

--- ArtPlatform_Test/CommentTest.cs
+++ ArtPlatform_Test/CommentTest.cs
@@ -1,19 +1,72 @@
+using ArtPlatform.Models;
 using NUnit.Framework;
+using System;
 
 namespace ArtPlatform_Test
 {
     public class CommentTest
     {
+        Comment Comment;
+        User User;
+        Post Post;
+        int Id;
+        DateTime Date;
+        string Text;
+
         [SetUp]
         public void Setup()
         {
+            Id = 1;
+            Text = "some text";
+            Date = DateTime.Now;
+            User = new User();
+            Post = new Post(Id, Text);
+            Comment = new Comment(Id, Text, Date, null, User, Post);
+
         }
 
         [Test]
-        public void Test1()
+        public void IdCommentTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(1, Comment.IdComment);
+            Comment.IdComment = 2;
+            Assert.AreEqual(2, Comment.IdComment);
         }
+
+        [Test]
+        public void CommentTextTest()
+        {
+            Assert.AreEqual("some text", Comment.CommentText);
+            Text = "some comment2";
+            Comment.CommentText = Text;
+            Assert.AreEqual(Text, Comment.CommentText);
+        }
+
+        [Test]
+        public void CreationDateTest()
+        {
+            Assert.AreEqual(Date, Comment.CreationDate);
+            Date = DateTime.Now.AddDays(120);
+            Comment.CreationDate = Date;
+            Assert.AreNotEqual(Date, Comment.CreationDate);
+        }
+
+
+        [Test]
+        public void EditDateTest()
+        {
+            Date = DateTime.Now.AddDays(1);
+            Comment.EditDate = Date;
+            Assert.AreEqual(Date, Comment.EditDate);
+
+            Date = DateTime.Now.AddDays(-30);
+            Comment.EditDate = Date;
+            Assert.AreNotEqual(Date, Comment.EditDate);
+        }
+
+        
+
+
 
     }
 }
--- ArtPlatform_Test/UserRatingTest.cs
+++ ArtPlatform_Test/UserRatingTest.cs
@@ -1,18 +1,45 @@
+using ArtPlatform.Models;
 using NUnit.Framework;
+using System;
 
 namespace ArtPlatform_Test
 {
     public class UserRatingTest
     {
+        int id = 1;
+        decimal value = 4;
+        string desc = "dqwdqw";
+        Post Post;
+        User User;
+        UserRating Rating;
+
         [SetUp]
         public void Setup()
         {
+            Rating = new UserRating(id, value, desc, Post, User);
         }
 
         [Test]
-        public void Test1()
+        public void IdRatingTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(id, Rating.IdRating);
+        }
+
+        [Test]
+        public void RatingValueTest()
+        {
+            Assert.AreEqual(value, Rating.RatingValue);
+            value = -10;
+            Exception ex = Assert.Throws<ArgumentException>(() => { Rating.RatingValue = value; });
+        }
+
+        [Test]
+        public void DescriptionTest()
+        {
+            Assert.AreEqual(desc, Rating.Description);
+            desc = "ddd";
+            Rating.Description = desc;
+            Assert.AreEqual(desc, Rating.Description);
         }
 
     }
